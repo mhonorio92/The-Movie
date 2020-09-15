17626db00c91a297c94af745b68bb5e0c99ed805
Marcelo Honorio <marceloshonorio@hotmail.com>
2020-09-15 12:24:33 -0300

    Added appicon and launchScreen Image

---
+++ The Movie/Feature/MovieDetail/View/MovieDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MovieDetailViewController.swift
+//  The Movie
+//
+//  Created by Marcelo Silva Honorio on 15/09/20.
+//  Copyright © 2020 Mhonorio92. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ The Movie/Main/Model/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  The Movie
+//
+//  Created by Marcelo Silva Honorio on 15/09/20.
+//  Copyright © 2020 Mhonorio92. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+            let homeViewController = MovieDetailViewController()
+            window!.rootViewController = homeViewController
+            window!.makeKeyAndVisible()
+            return true
+        }
+}
+
