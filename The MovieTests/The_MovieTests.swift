01ead85ff4a550eb1fdaebc80490b3495599f68b
Marcelo Honorio <marceloshonorio@hotmail.com>
2020-09-15 10:59:20 -0300

    Project creation

---
+++ The MovieTests/The_MovieTests.swift
@@ -0,0 +1,34 @@
+//
+//  The_MovieTests.swift
+//  The MovieTests
+//
+//  Created by Marcelo Silva Honorio on 15/09/20.
+//  Copyright © 2020 Mhonorio92. All rights reserved.
+//
+
+import XCTest
+@testable import The_Movie
+
+class The_MovieTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
